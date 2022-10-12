dd1136389200ac43785566ced03ab21d77ff777e
Wagner Leandro <leandro.loginw@gmail.com>
2022-10-12 19:20:11 -0300

    44-00-Stage2/6

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,38 @@
 // JSX: Javascript + XML (HTML)
 // Componentes / Propriedades
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <button>
-      {props.title}
-    </button>
-  )
-}
+import "./styles/main.css";
+import logoImg from "./assets/logo.svg";
 
 function App() {
   return (
-    <div>
-      <Button title="Send 1"/>
-      <Button title="Send 2"/>
-      <Button title="Send 3"/>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="logoNLW" />
+      
+      <h1 className="text-6xl text-white font-black mt-20">Seu <span className="bg-nlw-gradient bg-clip-text text-transparent">duo</span> está aqui.</h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="">
+          <img src="./public/game1.png" alt="" />
+        </a>
+        <a href="">
+          <img src="./public/game2.png" alt="" />
+        </a>
+        <a href="">
+          <img src="./public/game3.png" alt="" />
+        </a>
+        <a href="">
+          <img src="./public/game4.png" alt="" />
+        </a>
+        <a href="">
+          <img src="./public/game5.png" alt="" />
+        </a>
+        <a href="">
+          <img src="./public/game6.png" alt="" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
